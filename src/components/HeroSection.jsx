b99be33e1cc22263b31a14f2186247ed9b85ec0f
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-04-30 11:15:23 +0530

    updated font

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,9 +6,9 @@ const HeroSection = () => {
         <video autoPlay muted loop playsInline className='sm:hidden md:hidden block w-full h-full object-cover object-top' src="/showreel.mp4"></video>
         <video autoPlay muted loop playsInline className='sm:block md:block hidden w-full h-full object-cover object-top' src="/showreel.mp4"></video>
         <div className='w-full flex items-center justify-center flex-col h-screen absolute top-0 left-0'>
-            <img src="/logo.png" className='sm:h-[25vw] md:h-[15vw] lg:h-[12vw] h-[10vw] sm:top-[6vw] md:top-[4vw] lg:top-[4vw] md:left-[6vw] lg:left-[6vw] sm:left-1/2 sm:-translate-x-1/2 top-[2vw] left-[3.5vw] absolute' alt="" />
-            <h1 className='soon sm:text-[8vw] md:text-[7.2vw] lg:text-[6vw] xl:text-[4.8vw]  text-[3.5vw] uppercase' style={{fontWeight:"900"}}>Coming Soon</h1>
-            <p className='sm:text-[3vw] md:text-[2.8vw] lg:text-[2vw] xl:text-[1.8vw] text-[1.2vw] sm:font-semibold font-regular  font-[font1] sm:mb-[12vh]'>A Brand New Website Is Coming Soon!</p>
+            <img src="/logo.png" className='sm:h-[28vw] md:h-[15vw] lg:h-[12vw] h-[10vw] sm:top-[6vw] md:top-[4vw] lg:top-[4vw] md:left-[6vw] lg:left-[6vw] sm:left-1/2 sm:-translate-x-1/2 top-[2vw] left-[3.5vw] absolute' alt="" />
+            <h1 className='soon sm:text-[8.5vw] md:text-[7.2vw] lg:text-[6vw] xl:text-[4.8vw] text-[4vw] uppercase' style={{fontWeight:"900"}}>Coming Soon</h1>
+            <p className='sm:text-[3.5vw] md:text-[2.8vw] lg:text-[2vw] xl:text-[1.8vw] text-[1.2vw] sm:font-semibold font-regular  font-[font1] sm:mb-[12vh]'>A Brand New Website Is Coming Soon!</p>
         </div>
     </div>
   )
